85d44fd5d78280a4165c82a2198c8b3176af3bf9
devpla <chaeyeonhee@kakao.com>
2021-11-16 14:44:35 +0900

    feat: Apply dj-rest-auth library to accounts

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,40 +1,12 @@
-from django.contrib.auth import get_user_model
-from django.contrib.auth.password_validation import validate_password
-
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class UserSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-        )
-    nickname = serializers.CharField(
-        required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())]
-        )
-    
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    passwordConfirmation = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'nickname', 'password', 'passwordConfirmation')
-    
-    def validate(self, attrs):
-        if attrs['password'] != attrs['passwordConfirmation']:
-            raise serializers.ValidationError({"password": "Password fields didn't match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            nickname=validated_data['nickname'],
-            email=validated_data['email'],
-        )
-
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+        fields = ('id', 'email', 'password')
