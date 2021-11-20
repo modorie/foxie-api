99840e552f888492d5ec8b0ae65fa6b51a4079b7
devpla <chaeyeonhee@kakao.com>
2021-11-20 23:53:36 +0900

    feat: Add directors to movie serializer

--- movies/serializers/movie.py
+++ movies/serializers/movie.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from ..models import Genre, Movie, Actor, Casting
+from ..models import Genre, Movie, Actor, Casting, Director
 
 
 class MovieListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'poster_path',)
+        fields = ('id', 'title', 'poster_path', 'release_date', 'vote_average')
 
 
 class MovieSerializer(serializers.ModelSerializer):
@@ -16,7 +16,13 @@ class MovieSerializer(serializers.ModelSerializer):
             model = Genre
             fields = '__all__'
 
+    class DirectorSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Director
+            fields = '__all__'
+
     genre_ids = GenreSerializer(many=True, read_only=True)
+    directors = DirectorSerializer(many=True, read_only=True)
 
     class Meta:
         model = Movie
