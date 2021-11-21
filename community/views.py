0521ed89aa17fcf20bc199d1c110b449699b15b5
devpla <chaeyeonhee@kakao.com>
2021-11-21 20:09:13 +0900

    feat: Update view to get comment list

--- community/views.py
+++ community/views.py
@@ -14,6 +14,7 @@ def article_create_or_list(request):
         articles = Article.objects.all()
         serializer = ArticleListSerializer(articles, many=True)
         return Response(serializer.data)
+
     elif request.method == 'POST':
         serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -24,37 +25,49 @@ def article_create_or_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail_or_update_or_delete(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
+
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
+
     elif request.method == 'PUT':
         serializer = ArticleSerializer(data=request.data, instance=article)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
     else:
         article.delete()
         return Response('delete success', status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['POST'])
-def comment_create(request, article_pk):
+@api_view(['GET', 'POST'])
+def comment_create_or_list(request, article_pk):
     article = get_object_or_404(Article, pk=article_pk)
-    serializer = CommentSerializer(data=request.data)
-    if serializer.is_valid(raise_exception=True):
-        serializer.save(article=article)
+
+    if request.method == 'GET':
+        comments = article.comments.all()
+        serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
+
+    else:
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(article=article)
+            return Response(serializer.data)
 
 
 @api_view(['PUT', 'DELETE'])
 def comment_update_or_delete(request, article_pk, comment_pk):
-    # article = get_object_or_404(Article, pk=article_pk) - 불필요?
+    article = get_object_or_404(Article, pk=article_pk)
     comment = get_object_or_404(Comment, pk=comment_pk)
+
     if request.method == 'PUT':
         serializer = CommentSerializer(data=request.data, instance=comment)
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data)
+
     elif request.method == 'DELETE':
         comment.delete()
         return Response('delete success', status=status.HTTP_204_NO_CONTENT)
