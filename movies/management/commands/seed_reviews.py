b04f03a77d4db4b086d11225a8eb745e8dda38ae
devpla <chaeyeonhee@kakao.com>
2021-11-22 11:36:33 +0900

    feat: Update seed commands to get more data

--- movies/management/commands/seed_reviews.py
+++ movies/management/commands/seed_reviews.py
@@ -11,14 +11,14 @@ User = get_user_model()
 
 
 class Command(BaseCommand):
-    help = "이 커맨드를 통해 랜덤한 리뷰 및 댓글 데이터를 만듭니다."
+    help = "이 커맨드를 통해 랜덤한 영화 리뷰 댓글 데이터를 만듭니다."
 
     def add_arguments(self, parser):
         parser.add_argument(
             "--number",
             default=2,
             type=int,
-            help="몇 개의 리뷰 및 댓글 데이터를 만들것인지"
+            help="몇 개의 글 데이터를 만들것인지"
         )
 
     def handle(self, *args, **options):
@@ -28,25 +28,28 @@ class Command(BaseCommand):
         users = User.objects.all()
         movies = Movie.objects.all()
 
-        seeder.add_entity(
-            Review,
-            number,
-            {
-                "movie": lambda x: random.choice(movies),
-                "author": lambda x: random.choice(users),
-                "rank": lambda x: random.randint(1, 10),
-            }
-        ),
-
-        seeder.execute()
-
-        seeder = Seed.seeder()
+        # 리뷰 데이터를 생성하려면 아래 코드를 활성화합니다.
+        # seeder.add_entity(
+        #     Review,
+        #     number,
+        #     {
+        #         "movie": lambda x: random.choice(movies),
+        #         "author": lambda x: random.choice(users),
+        #         "rank": lambda x: random.randint(1, 10),
+        #     }
+        # ),
+        #
+        # seeder.execute()
+        #
+        # seeder = Seed.seeder()
         reviews = Review.objects.all()
 
+        # 리뷰 좋아요 추가
         for review in reviews:
             for i in range(random.randint(0, 6)):
                 review.like_users.add(random.choice(users))
 
+        # 댓글 추가
         seeder.add_entity(
             Comment,
             number,
@@ -57,11 +60,10 @@ class Command(BaseCommand):
         )
 
         seeder.execute()
-
         comments = Comment.objects.all()
 
         for comment in comments:
             for i in range(random.randint(0, 6)):
                 comment.like_users.add(random.choice(users))
 
-        self.stdout.write(self.style.SUCCESS(f"{number}개의 리뷰와 댓글이 작성되었습니다."))
+        self.stdout.write(self.style.SUCCESS(f"{number}개의 댓글이 작성되었습니다."))
