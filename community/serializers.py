cbf79a907ecc4f0def41854c729d92e6899795b6
devpla <chaeyeonhee@kakao.com>
2021-11-22 11:40:44 +0900

    feat: Add more fields to community serializer

--- community/serializers.py
+++ community/serializers.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Article, Comment
+
+User = get_user_model()
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -18,8 +21,22 @@ class ArticleSerializer(serializers.ModelSerializer):
 
 
 class ArticleListSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(many=True, read_only=True)
+    comments_count = serializers.IntegerField(
+        source='comments.count',
+        read_only=True
+    )
+    likes_count = serializers.IntegerField(
+        source='like_users.count'
+    )
+
+    class UserSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = User
+            fields = ('id', 'username')
+
+    author = UserSerializer(read_only=True)
 
     class Meta:
         model = Article
-        fields = ('id', 'title', 'author',)
+        fields = ('id', 'title', 'author', 'comments_count', 'created_at', 'likes_count')
