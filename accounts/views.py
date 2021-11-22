35de537d699bc8f0f92b37653f8c63e925a1a68c
devpla <chaeyeonhee@kakao.com>
2021-11-23 00:55:23 +0900

    feat: Add movies data to profile

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,25 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, UserSerializer
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+    password = request.data.get('password')
+    password_confirmation = request.data.get('passwordConfirmation')
+
+    if password != password_confirmation:
+        return Response({'error': '비밀번호가 일치하지 않습니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid(raise_exception=True):
+        user = serializer.save()
+        user.set_password(request.data.get('password'))
+        user.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'PUT'])
